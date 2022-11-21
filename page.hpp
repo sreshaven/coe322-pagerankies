bc4fad39201d1fb1b2d0d8b1e10e8515d6e1ed16
Sresha Ventrapragada <sreshaven@gmail.com>
2022-11-21 10:23:43 -0600

    starting page class

---
+++ page.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "link.hpp"
+#pragma once
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::shared_ptr;
+using std::make_shared;
+
+class Page {
+	private:
+		string name;
+		vector<shared_ptr<Page>> linksVector;
+	public:
+		Page(string inputName){
+			name = inputName;
+		}
+
+		string getName(){
+			return name;
+		}
+		
+		string as_string(){
+			string output = "";
+			output += name + '\n';
+			for (int i = 0; i < linksVector.size(); i++){
+				output += linksVector[i]->getName();
+				if (i != linksVector.size() - 1){
+					output += '\n';
+				}
+			}
+			return output
+		}
+	
+		void add_link(shared_ptr<Page> linkedPage){
+			linksVector.push_back(linkedPage);		
+		}
+	
+		shared_ptr<Page> random_click(){
+			if(linksVector.empty()){
+				return Page("this page has no links")		
+			}
+			else{
+				return linksVector[rand() % linksVector.size()]
+			}
+		}
+
+}
